952e6e4f703892526dfc7a18eafe14f2e9c0a159
Nick Fletcher <nickeax@gmail.com>
2021-09-27 20:42:35 +1000

    Set up base and abstract classes and interfaces.

---
+++ OnlineShop/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace OnlineShop.Repositories
+{
+    public abstract class BaseRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        protected BaseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
---
+++ OnlineShop/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<int> CreateAsync(T entity);
+        Task<int> UpdateAsync(T entity);
+        Task<int> DeleteAsync(T entity);
+    }
+}
